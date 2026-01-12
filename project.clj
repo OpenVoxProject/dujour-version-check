add852d7e69fd28153c4b3ce592bce19edc6e145
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-12 22:25:02 +0000

    Version 1.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/dujour-version-check "1.1.2"
+(defproject org.openvoxproject/dujour-version-check "1.1.3-SNAPSHOT"
   :description "Dujour Version Check library"
   :license {:name "Apache-2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
