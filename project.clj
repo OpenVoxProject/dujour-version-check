f67f399af603bb994203c75611d61b8308c23b82
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-12 12:59:18 -0800

    Move version pins
    This moves the version pinning into managed-deps to avoid endless conflicts due to :pedantic? abort.

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
-(def trapperkeeper-version "4.3.2")
-(def trapperkeeper-webserver-jetty10-version "1.1.2")
-
 (defproject org.openvoxproject/dujour-version-check "1.1.2-SNAPSHOT"
   :description "Dujour Version Check library"
   :license {:name "Apache-2.0"
@@ -9,35 +6,45 @@
   :pedantic? :abort
   :min-lein-version "2.9.1"
 
-  ;; These are to enforce consistent versions across dependencies of dependencies,
-  ;; and to avoid having to define versions in multiple places. If a component
-  ;; defined under :dependencies ends up causing an error due to :pedantic? :abort,
-  ;; because it is a dep of a dep with a different version, move it here.
+  ;; Generally, try to keep version pins in :managed-dependencies and the libraries
+  ;; this project actually uses in :dependencies, inheriting the version from
+  ;; :managed-dependencies. This prevents endless version conflicts due to deps of deps.
+  ;; Renovate should keep the versions largely in sync between projects.
   :managed-dependencies [[org.clojure/clojure "1.12.4"]
-                         [commons-codec "1.20.0"]
+                         [org.clojure/tools.logging "1.3.1"]
+                         [cheshire "5.13.0"]
                          [clj-time "0.15.2"]
+                         [commons-codec "1.20.0"]
+                         
                          [org.slf4j/slf4j-api "2.0.17"]
+                         [prismatic/schema "1.4.1"]
+                         [ring-mock "0.1.5"]
+                         [ring/ring-codec "1.3.0"]
+                         [slingshot "0.12.2"]
+                         [trptcolin/versioneer "0.2.0"]
+                         
 
                          [org.bouncycastle/bcpkix-jdk18on "1.83"]
                          [org.bouncycastle/bcpkix-fips "1.0.8"]
                          [org.bouncycastle/bc-fips "1.0.2.6"]
                          [org.bouncycastle/bctls-fips "1.0.19"]
-
-                         [org.openvoxproject/trapperkeeper ~trapperkeeper-version]
-                         [org.openvoxproject/trapperkeeper ~trapperkeeper-version :classifier "test"]
-                         [org.openvoxproject/trapperkeeper-webserver-jetty10 ~trapperkeeper-webserver-jetty10-version]
-                         [org.openvoxproject/trapperkeeper-webserver-jetty10 ~trapperkeeper-webserver-jetty10-version :classifier "test"]]
+                         [org.openvoxproject/http-client "2.2.2"]
+                         [org.openvoxproject/kitchensink "3.5.5" :classifier "test" :scope "test"]
+                         [org.openvoxproject/trapperkeeper "4.3.2"]
+                         [org.openvoxproject/trapperkeeper "4.3.2" :classifier "test"]
+                         [org.openvoxproject/trapperkeeper-webserver-jetty10 "1.1.2"]
+                         [org.openvoxproject/trapperkeeper-webserver-jetty10 "1.1.2" :classifier "test"]]
 
   :dependencies [[org.clojure/clojure]
-                 [org.clojure/tools.logging "1.3.1"]
+                 [org.clojure/tools.logging]
 
-                 [prismatic/schema "1.4.1"]
-                 [ring/ring-codec "1.3.0"]
-                 [cheshire "5.13.0"]
-                 [trptcolin/versioneer "0.2.0"]
-                 [slingshot "0.12.2"]
+                 [prismatic/schema]
+                 [ring/ring-codec]
+                 [cheshire]
+                 [trptcolin/versioneer]
+                 [slingshot]
 
-                 [org.openvoxproject/http-client "2.2.2"]]
+                 [org.openvoxproject/http-client]]
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/CLOJARS_USERNAME
@@ -45,11 +52,11 @@
                                      :sign-releases false}]]
 
   :profiles {:provided {:dependencies [[org.bouncycastle/bcpkix-jdk18on]]}
-             :defaults {:dependencies [[org.openvoxproject/trapperkeeper :classifier "test" :scope "test"]
-                                       [org.openvoxproject/kitchensink "3.5.5" :classifier "test" :scope "test"]
+             :defaults {:dependencies [[org.openvoxproject/kitchensink :classifier "test" :scope "test"]
+                                       [org.openvoxproject/trapperkeeper :classifier "test" :scope "test"]
                                        [org.openvoxproject/trapperkeeper-webserver-jetty10]
                                        [org.openvoxproject/trapperkeeper-webserver-jetty10 :classifier "test"]
-                                       [ring-mock "0.1.5"]]}
+                                       [ring-mock]]}
                                   
              :dev-settings {:dependencies [[org.bouncycastle/bcpkix-jdk18on]]}
              :dev [:defaults :dev-settings]
